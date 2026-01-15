cc304f96f1c170aa5df4ac7e348a413a510b30dd
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-01-16 00:06:39 +0600

    security configured

---
+++ src/main/java/com/arpon007/netflixclone/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.arpon007.netflixclone.config;
+
+import com.arpon007.netflixclone.Security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
+    private static final String[] PUBLIC_URLS = {
+            "/api/auth/**",
+            "/api/files/image/**",
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // NoOpPasswordEncoder is deprecated; consider using a strong encoder like BCryptPasswordEncoder.
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PUBLIC_URLS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+}
